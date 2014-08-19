bbff11149151563a8f7c47e9d644ac20686c2b08
mattwilliams & Andrew M Westling <student@epicodus.com>
2014-08-19 12:30:04 -0700

    add presence validation for contacts

---
+++ db/migrate/20140819184311_contacts.rb
@@ -0,0 +1,11 @@
+class Contacts < ActiveRecord::Migration
+  def change
+    create_table :contacts do |t|
+      t.column :name, :string
+      t.column :phone, :string
+      t.column :email, :string
+
+      t.timestamps
+    end
+  end
+end
